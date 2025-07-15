0f477fbdbdd97d0e4b906a4fc84cc6951d4811eb
breezynotcheezy <87101443+breezynotcheezy@users.noreply.github.com>
2025-07-15 10:42:36 -0400

    Delete app directory

--- app/api/v1/scan/batch/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest } from 'next/server'
-import { proxy } from '@/app/api/_proxy'
-
-export async function POST(req: NextRequest) {
-  const data = await req.formData()
-  return proxy('/api/v1/scan/batch', { method: 'POST', body: data })
-}
